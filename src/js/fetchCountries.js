f3c9d0c3d5949b9e7ad52b1361dd56ef3359ad6b
Yaroslava-Vorobets <slavavor85@gmail.com>
2022-10-14 12:27:20 +0200

    markup

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,8 @@
 
 export class FetchCountries {
 
-  getcountries(inputValue) {
-    const url = 'https://restcountries.com/v3/all?fields=name,capital,population,flags,languages'
+  getCountries(inputValue) {
+     const url =  `https://restcountries.com/v3.1/name/${inputValue}?fields=name,capital,population,flags,languages`
     return fetch(url)
       .then(response => {
         if (!response.ok) {
@@ -10,39 +10,10 @@ export class FetchCountries {
         }
         return response.json();
       })
-      // .then(name => {
-      //   createMarkup({ name })
-      //   return
-      // })
-      // .catch(error => {
-      //   console.log(error)
-      // });
-
   }
 }
 
-  function createMarkup({ name, capital, population, flags, languages }) {  
-  console.log(name[0].name.official)
-  console.log(name[0].capital)
-  console.log(name[0].population)
-  console.log(name[0].flags)
-  console.log(name[0].languages)
-  //  name.map(({ name, flags }) => {
-  //    console.log(name[0].name.official)
-  //    return ` <li class="country-list__item">
-  //     <img
-  //       class="country-list__flag"
-  //       src="${flags.svg}"
-  //       width="30px"
-  //       height="20px"
-  //     />
-  //     <p class="country-list__name">${name.official}</p>
-  //   </li>`;
-  // }).join()
-
-  }
-  
-     
+ 
  
   
 
--- src/js/markup.js
+++ src/js/markup.js
@@ -1,30 +1,28 @@
 export function markupCountriesList(name) {
 
-  return name.map(({ name, flags }) => {  
+   return name.map(({ name, flags }) => {  
      return ` <li class="country-list__item">
-      <img
-        class="country-list__flag"
+      <img       
         src="${flags.svg}"
-        width="30px"
-        height="20px"
+        width="40px"
+        height="30px"
       />
       <p class="country-list__name">${name.official}</p>
     </li>`;
-  }).join()  
+  }).join('')  
 
 }
 
-export function markupCountries(name) {
-  return name.map(({ name, capital, population, flags, languages }) => {
-    return ` <div class="country-info_head">
-        <img src="${flags.svg}" alt="flag" class="country-info_flag">
+export function markupCountries({ name, capital, population, flags, languages }) { 
+     return ` <div class="country-info_head">
+        <img src="${flags.svg}"  width="40px" height="30px" alt="flag">
         <p class="country-info_capital">${name.official}</p>
       </div>
       <div class="description">
-        <p class="description_capital">Capital: <span class="description__name.capital">${capital}</span> </p>
-        <p class="description_population">Population: <span class="description__name.population">${population}</span> </p></p>
-        <p class="description_languages">Languages: <span class="description__name.languages">${languages}</span> </p></p>
+        <p class="description_capital">Capital: <span class="description__name-capital">${capital}</span> </p>
+        <p class="description_population">Population: <span class="description__name-population">${population}</span> </p></p>
+        <p class="description_languages">Languages: <span class="description__name-languages">${Object.values(languages).join('')}</span> </p></p>
       </div>`
-  })
+ 
 
 }
